541f695ca33eee31a9c62d5c128858288415b711
David Warry <dave.warry@live.com>
2024-12-01 16:05:11 +0000

    Add project files.

---
+++ AdventOfCode2024/Day1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024;
+internal static class Day1
+{
+    public static void Part1(string path)
+    {
+        List<int> list1 = [];
+        List<int> list2 = [];
+
+        foreach(string line in File.ReadLines(path))
+        {
+            var parts = Regex.Split(line, @"\s+");
+
+            list1.Add(Convert.ToInt32(parts[0]));
+            list2.Add(Convert.ToInt32(parts[1]));
+        }
+
+        list1.Sort();
+        list2.Sort();
+
+        var result = list1.Zip(list2).Select(tup => Math.Abs(tup.First - tup.Second)).Sum();
+
+        Console.WriteLine($"Part 1: {result}");
+
+        Dictionary<int, int> counts = [];
+
+        foreach(var i in list2)
+        {
+            if(counts.ContainsKey(i))
+            {
+                counts[i] += i;
+            }
+            else
+            {
+                counts[i] = i;
+            }
+        }
+        
+        var similarityScore = list1.Sum(x => counts.GetValueOrDefault(x, 0));
+
+        Console.WriteLine(similarityScore);
+    }
+}
---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventOfCode2024;
+
+Console.WriteLine("Advent of Code 2024!");
+
+Day1.Part1(@"D:\dev\vs2022\AdventOfCode2024\data\day1.txt");
