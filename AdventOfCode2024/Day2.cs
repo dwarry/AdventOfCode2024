e4e4758c6441fc41cff046a1f0d7413a5efd7c09
David Warry <dave.warry@live.com>
2024-12-02 08:25:00 +0000

    Removed redundant example dataset

--- AdventOfCode2024/Day2.cs
+++ AdventOfCode2024/Day2.cs
@@ -8,13 +8,6 @@ using System.Threading.Tasks;
 namespace AdventOfCode2024;
 internal static class Day2
 {
-    readonly static string[] data = @"7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9".Split("\r\n");
-
     public static void Solve(string path)
     {
 
